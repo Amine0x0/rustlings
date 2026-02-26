5fdbc5e253be290a6815108ee86d0dfb03adf2be
l0w <orbit@192.168.11.112>
2026-02-26 05:53:22 +0000

    solve...

---
+++ 04_primitive_types/primitive_types3.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // TODO: Create an array called `a` with at least 100 elements in it.
+    // let a = ???
+    let a = ["sus?"; 100];
+    {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
---
+++ 04_primitive_types/primitive_types5.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+
+    let name = cat.0;
+    let age = cat.1;
+
+    println!("{name} is {age} years old");
+}
